41f2cce8953184c33ff22b736d0c07e75f0ae0cb
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2019-12-23 22:29:46 -0300

    Ajsut some pages and start rspec study

--- ciliatest/app/helpers/site_helper.rb
+++ ciliatest/app/helpers/site_helper.rb
@@ -2,9 +2,12 @@ module SiteHelper
 
   def products_on_cart
     unless current_client.cart.nil?
-      @cart_count = (current_client.cart.cart_products).count
       @products_on_cart = current_client.cart.products
     end
+  end
+
+  def cart_count
+    @cart_count = (current_client.cart.cart_products).count
   end
 
 end
---
+++ ciliatest/spec/lib/hero_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require_relative 'hero'
+
+describe Hero do
+  it "has a sword"
+end
